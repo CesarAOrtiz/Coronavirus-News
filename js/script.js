9cafdd13b147e6721283f9f07410631b282760cb
César Ortiz <68305906+CesarAOrtiz@users.noreply.github.com>
2020-09-29 00:10:42 -0400

    Arreglando tiempo de carga

--- js/script.js
+++ js/script.js
@@ -18,24 +18,24 @@ const callAPI = async () => {
         const data = await response.json();
         saveResponse(data);
         showAll();
+        activateLinks();
     } catch (error) {
-        console.log(error);
+        document.getElementById("not-found").style.display = "block";
     }
 };
 
 function saveResponse(response) {
     var data = [];
-    var iter = 0;
-    for (let object of response.Countries) {
+    response.Countries.forEach((object, index) => {
         data.push({
+            Total_Confirmed: object.TotalConfirmed.toLocaleString("es-MX"),
+            Total_Deaths: object.TotalDeaths.toLocaleString("es-MX"),
+            Total_Recovered: object.TotalRecovered.toLocaleString("es-MX"),
             Country: object.Country,
             Current_Infected: (
                 object.TotalConfirmed -
                 (object.TotalDeaths + object.TotalRecovered)
             ).toLocaleString("es-MX"),
-            Total_Confirmed: object.TotalConfirmed.toLocaleString("es-MX"),
-            Total_Deaths: object.TotalDeaths.toLocaleString("es-MX"),
-            Total_Recovered: object.TotalRecovered.toLocaleString("es-MX"),
             New_Confirmed: object.NewConfirmed.toLocaleString("es-MX"),
             New_Deaths: object.NewDeaths.toLocaleString("es-MX"),
             New_Recovered: object.NewRecovered.toLocaleString("es-MX"),
@@ -48,17 +48,31 @@ function saveResponse(response) {
                 100
             ).toFixed(2),
             Date: String(object.Date).slice(0, 10),
-            id: iter,
+            id: index,
         });
-        iter++;
-    }
+    });
     countries = data;
 }
 
 function showAll() {
-    table = createBlocks(countries);
-    document.getElementById("sub-container").innerHTML = table;
-    activateLinks();
+    let html = "";
+    for (let object of countries) {
+        html += `
+    <div class="block" id="${object.id}">
+        <p class='detail-link background'>${object.Country}</p>
+        <ul class="header">
+            <li class="header-digits">Total Confirmed</li>
+            <li class="header-digits">Total Deaths</li>
+            <li class="header-digits">Total Recovered</li>
+        </ul>
+        <ul>
+            <li>${object.Total_Confirmed}</li>
+            <li>${object.Total_Deaths}</li>
+            <li>${object.Total_Recovered}</li>
+        </ul>
+    </div>`;
+    }
+    document.getElementById("sub-container").innerHTML = html;
 }
 
 function activateLinks() {
@@ -96,27 +110,6 @@ function activateLinks() {
     );
 }
 
-function createBlocks(array) {
-    let html = "";
-    for (let object of array) {
-        html += `
-    <div class="block">
-        <p id="${object.id}" class='detail-link background'>${object.Country}</p>
-        <ul class="header">
-            <li class="header-digits">Total Confirmed</li>
-            <li class="header-digits">Total Deaths</li>
-            <li class="header-digits">Total Recovered</li>
-        </ul>
-        <ul>
-            <li>${object.Total_Confirmed}</li>
-            <li>${object.Total_Deaths}</li>
-            <li>${object.Total_Recovered}</li>
-        </ul>
-    </div>`;
-    }
-    return html;
-}
-
 function showSearch(e) {
     e.preventDefault();
     let search = document
@@ -127,11 +120,11 @@ function showSearch(e) {
         object.Country.toLowerCase().includes(search)
     );
     const results = coincidense.map((object) => String(object.id));
-    document.querySelectorAll(".block p").forEach((object) => {
+    document.querySelectorAll(".block").forEach((object) => {
         if (results.includes(object.id)) {
-            object.parentNode.style.display = "block";
+            object.style.display = "block";
         } else {
-            object.parentNode.style.display = "none";
+            object.style.display = "none";
         }
     });
     if (results.length < 1) {
@@ -143,7 +136,7 @@ function showSearch(e) {
 
 function showDetails(e) {
     e.preventDefault();
-    object = countries[e.currentTarget.id];
+    object = countries[e.target.parentNode.id];
     document.getElementById("Country").textContent = object.Country;
     document.getElementById("Date").textContent = object.Date;
     document.getElementById("TConfirmed").textContent = object.Total_Confirmed;
